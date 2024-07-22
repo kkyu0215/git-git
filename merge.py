870a1c92b9556fee513261dcb9225d99afbcf5d0
chanwoomoon <2020143514@yonsei.ac.kr>
2024-07-22 17:57:16 +0900

    merge22

--- merge.py
+++ merge.py
@@ -1,8 +1,5 @@
-# merge.py
 def greet():
-print("Hello from the chanwoo branch") 
-#이부분에서 충돌이 발
-#것이므로 gildong부분에 자기 이름 넣어주시면 됩니다.
+print("Hello from the chanwoo1 branch")
 def add(a, b):
 return a + b
 def subtract(a, b):
@@ -19,9 +16,6 @@ greet()
 result_add = add(10, 5)
 result_sub = subtract(10, 5)
 result_mul = multiply(10, 5)
-
-Git 협업 과제 8
-
 result_div = divide(10, 5)
 print(f"Add: {result_add}, Subtract: {result_sub}, Multipl
 if __name__ == "__main__":
